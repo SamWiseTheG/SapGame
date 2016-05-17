429360aff40816f6a2fdc7ccf7ca9dfa7fff3905
Sam Goldstein <github@samuelgoldstein.com>
2016-05-17 16:13:46 -0700

    Second Commit

---
+++ src/application/Platform.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.ArrayList;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Platform extends RectangleComponent
+{
+	protected static ArrayList<Platform> platformsArray = new ArrayList<Platform>();
+	//Rectangle component;
+	
+	public Platform(Group componentsGroup, double x, double y, double width, double height, Color color) 
+	{
+		this(componentsGroup, x, y, width);
+		component.setHeight(height);
+		component.setFill(color);
+		platformsArray.add(0, this);
+	}
+	
+	public Platform(Group componentsGroup, double x, double y, double width) 
+	{
+		//Set up component
+		component = new Rectangle();
+		component.setWidth(width);
+		component.setHeight(20);
+		component.setFill(Color.DARKMAGENTA);
+		component.setTranslateX(x);
+		component.setTranslateY(y);
+		
+		//Add Platform ArrayList
+		platformsArray.add(0, this);
+		
+		//Add component to root of passed in scene
+		componentsGroup.getChildren().add(component);
+	}
+	public static ArrayList<Platform> getPlatformsArrayList() 
+	{
+		return platformsArray;
+	}
+	
+	public static void reset() 
+	{
+		platformsArray.clear();
+	}
+}
---
+++ src/application/PowerUp.java
@@ -0,0 +1,94 @@
+package application;
+
+import java.util.ArrayList;
+import javafx.geometry.Bounds;
+import javafx.scene.*;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+
+public class PowerUp
+{
+	// the shape of the power up
+	Circle component;
+	// the group sets precedent of the created objects
+	Group powerUpGroup;
+	// the list of all the power ups
+	protected static ArrayList<PowerUp> powerUpArray = new ArrayList<PowerUp>();
+	private int type;
+	
+	//constructor   precedent,  xcord,    ycord, what powerup gives
+	public PowerUp( Group g, double x, double y, int type )
+	{
+		powerUpGroup = g;
+		component = new Circle();
+		powerUpArray.add( 0, this );
+		component.setRadius( 15 );
+		component.setTranslateX( x );
+		component.setTranslateY( y );
+		if( type == 1 )
+		{
+			component.setFill( Color.FORESTGREEN );
+			setType(1);
+		}
+		else if( type == 2 )
+		{
+			component.setFill( Color.DARKBLUE );
+			setType(2);
+		}
+		else if( type == 3 )
+		{
+			component.setFill( Color.CRIMSON );
+			setType(3);
+		}
+		g.getChildren().add( component );
+	}
+	
+	// for unit collision
+	public Bounds getBounds() 
+	{
+		return component.getBoundsInParent();
+	}
+	public void setType(int type) 
+	{
+		this.type = type;
+	}
+	public int getType() 
+	{
+		return this.type;
+	}
+	
+	// remove from the array list
+	public void delete() 
+	{
+		powerUpGroup.getChildren().remove( component );
+		powerUpArray.remove(this);
+
+	}
+	
+	// getter of the array list
+	public static ArrayList<PowerUp> getPowerUpArrayList() 
+	{
+		return powerUpArray;
+	}
+	
+	// resets the array list
+	public static void resetArrayList() 
+	{
+		powerUpArray.clear();
+	}
+	
+	// increase jump height = 1 green
+		
+	// invincibility = 2 blue
+	
+	// extra life = 3 red
+	
+//	public static void main ( String [] args )
+//	{
+//		Group root = new Group();
+//		Canvas c = new Canvas( 300, 300 );
+//		new PowerUp( root, 2, 5, 1 );
+//		root.getChildren().add( c );
+//	}
+}
